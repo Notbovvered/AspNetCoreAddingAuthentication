a30c9f19cf3f48b50bbe1023821602754abfcee0
Joseph <Jkhassan95@gmail.com>
2020-07-28 10:40:25 +0100

    Added _userManager to item controller

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -1,13 +1,16 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Data;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
     [Authorize]
     public class ItemController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ApplicationDbContext _context;
 
         public ItemController(ApplicationDbContext context)
